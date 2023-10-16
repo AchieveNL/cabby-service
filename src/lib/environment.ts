45dfb4b83efdc5977f6533902c0499931db1eeb9
Oussama Bentaib <obenjrtaib@gmail.com>
2023-10-16 01:42:52 +0100

    feat: load env file

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,11 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import { config as configDotenv } from 'dotenv';
 import { cleanEnv } from 'envalid';
-import { EnvironmentFile, Environments } from '@/enums/environment.enum';
+import { Environments } from '@/enums/environment.enum';
 import envValidationConfig from '@/config/env-validation.config';
-import { envFileNotFoundError } from '@/utils/helper';
-import { type CommonEnvKeys } from '@/types/environment.type';
 import appConfig from '@/config/app.config';
 
 export interface IEnvironment {
@@ -51,18 +47,6 @@ class Environment implements IEnvironment {
     this._appUrl = value;
   }
 
-  private resolveEnvPath(key: CommonEnvKeys): string {
-    // On priority bar, .env.[NODE_ENV] has higher priority than default env file (.env)
-    // If both are not resolved, error is thrown.
-    const rootDir: string = path.resolve(__dirname, '../../');
-    const envPath = path.resolve(rootDir, EnvironmentFile[key]);
-    const defaultEnvPath = path.resolve(rootDir, EnvironmentFile.DEFAULT);
-    if (!fs.existsSync(envPath) && !fs.existsSync(defaultEnvPath)) {
-      throw new Error(envFileNotFoundError(key));
-    }
-    return fs.existsSync(envPath) ? envPath : defaultEnvPath;
-  }
-
   private validateEnvValues() {
     const env = cleanEnv(process.env, envValidationConfig);
     this.port = env.PORT;
@@ -72,12 +56,8 @@ class Environment implements IEnvironment {
   public setEnvironment(env = Environments.DEV): void {
     this.env = env;
 
-    const envKey = Object.keys(Environments).find(
-      (key) => Environments[key] === this.env
-    ) as keyof typeof Environments;
-    const envPath = this.resolveEnvPath(envKey);
-
-    configDotenv({ path: envPath });
+    // Just load the .env file
+    configDotenv();
     this.validateEnvValues();
   }
 
