d59fe36b8cadc87e5c592aff126838b89482163a
Oussama Bentaib <obenjrtaib@gmail.com>
2023-10-20 20:49:07 +0100

    feat: rental agreement and login adjustment

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,6 +3,6 @@ import { Storage } from '@google-cloud/storage';
 export const bucketName = 'cabby-bucket';
 
 export const gStorage = new Storage({
-  credentials: JSON.parse(process.env.GCP_CREDENTIALS as string),
+  credentials: JSON.parse(process.env.GC_CREDENTIALS_JSON as string),
   projectId: 'cabby-392012',
 });
