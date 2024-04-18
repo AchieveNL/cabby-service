9b3f6a970b0706f7d6b7888113ad8d5d2bc9d685
Kareem Khaled <kareemkhaled143@gmail.com>
2024-04-18 14:07:49 +0200

    feat: fix cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ export const determineCorsOrigin = () => {
     case 'production':
       return 'https://dashboard.cabbyrentals.com';
     case 'staging':
-      return '*';
+      return 'https://dashboard-staging.cabbyrentals.com';
     case 'development':
       return 'http://localhost:3000';
     default:
