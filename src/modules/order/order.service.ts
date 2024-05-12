e70719e79d02eb85867a2b1bec29590b6380a65f
Oussama BenTaib <obenjrtaib@gmail.com>
2024-05-12 22:51:01 +0200

    feat: include payment in order details

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -87,7 +87,7 @@ export default class OrderService {
   public getOrderDetailsWithStatus = async (orderId: string) => {
     const order = await prisma.order.findUnique({
       where: { id: orderId },
-      include: { vehicle: true },
+      include: { vehicle: true, payment: true },
     });
 
     if (!order) {
