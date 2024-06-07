e5bc3a3cf00f2d5c2024156bfaf4f36187632024
MrAbdul <monimesamadi@gmail.com>
2024-06-08 00:22:24 +0100

    fix: timeframes calculation

--- src/modules/order/functions.ts
+++ src/modules/order/functions.ts
@@ -30,33 +30,30 @@ function calculateTimeframes(startDate: Date, endDate: Date) {
   while (start <= end) {
     const day = dayjsExtended.utc(start).get('day');
     const row = day - 1;
-    // console.log("Row", row);
     const trueRow = row >= 0 ? row : 6;
-    // console.log("truerow", trueRow);
 
     for (const [column, timeframe] of timeframes.entries()) {
-      // console.log("Column", column);
       const one = timeframe[0];
       const two = timeframe[1];
       const timeframeStart = dayjsExtended
         .utc(start)
         .set('hours', one)
+        .set('m', 0)
         .toDate();
-      const timeframeEnd = dayjsExtended.utc(start).set('hours', two).toDate();
-
+      const timeframeEnd = dayjsExtended
+        .utc(start)
+        .set('hours', two)
+        .set('m', 0)
+        .toDate();
       let value = 0;
       if (!(endDate <= timeframeStart || startDate >= timeframeEnd)) {
         if (timeframeStart >= startDate && timeframeEnd <= endDate) {
-          // console.log('1');
           value = 6;
         } else if (timeframeStart <= startDate && endDate <= timeframeEnd) {
-          // console.log('2');
           value = durationInMilli(startDate, endDate);
         } else if (timeframeEnd >= endDate) {
-          // console.log('3');
           value = durationInMilli(timeframeStart, endDate);
         } else if (timeframeStart <= startDate) {
-          // console.log('4');
           value = durationInMilli(start, timeframeEnd);
         }
 
@@ -78,10 +75,8 @@ export function calculateOrderPrice(
   startDate = new Date(startDate);
   endDate = new Date(endDate);
 
-  // console.log({ startDate, endDate, pricing });
   const timeframes = calculateTimeframes(startDate, endDate);
-  // console.log(timeframes);
-
+  // console.log({ pricing, timeframes });
   let price = 0;
 
   pricing.forEach((row, rowIndex) => {
