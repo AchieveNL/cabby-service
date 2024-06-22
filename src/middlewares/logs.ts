4422e546e7a9a58c1a6867ca87efb409b1702807
MrAbdul <monimesamadi@gmail.com>
2024-06-22 17:15:13 +0100

    feat: added ip address to logs

--- src/middlewares/logs.ts
+++ src/middlewares/logs.ts
@@ -8,13 +8,13 @@ export const logsMiddleware = async (
 ) => {
   const userId = req.user?.id || null;
   const { headers, body, query, params, method, url, hostname, ip } = req;
-  const ipAddress =
-    req.headers['x-forwarded-for'] ?? req.socket.remoteAddress ?? ip;
+  const ipAddress = (req.headers['x-forwarded-for'] ??
+    req.socket.remoteAddress) as string;
 
   const data = { hostname, ip, ipAddress, headers };
   prisma.logs
     .create({
-      data: { userId, data, method, url, body, params, query },
+      data: { userId, data, method, url, body, params, query, ip: ipAddress },
     })
     .catch((err) => {
       console.log('Error saving log', err);
