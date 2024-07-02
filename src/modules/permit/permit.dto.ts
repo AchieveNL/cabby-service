a031f0bc712269b06a00c788d6a03cd863d611ff
MrAbdul <monimesamadi@gmail.com>
2024-07-02 12:09:15 +0100

    fix: changed kvk_number to integer type

--- src/modules/permit/permit.dto.ts
+++ src/modules/permit/permit.dto.ts
@@ -1,5 +1,5 @@
 // permit.dto.ts
-import { IsString, IsOptional } from 'class-validator';
+import { IsString, IsOptional, IsNumber } from 'class-validator';
 
 export class CreatePermitDto {
   @IsString()
@@ -10,9 +10,9 @@ export class CreatePermitDto {
   @IsOptional()
   kvkDocument?: string;
 
-  @IsString()
+  @IsNumber()
   @IsOptional()
-  kvkNumber?: string;
+  kvkNumber?: number;
 
   @IsString()
   @IsOptional()
@@ -40,9 +40,9 @@ export class UpdatePermitDto {
   @IsOptional()
   kvkDocumentId?: string;
 
-  @IsString()
+  @IsNumber()
   @IsOptional()
-  kvkNumber?: string;
+  kvkNumber?: number;
 
   @IsString()
   @IsOptional()
