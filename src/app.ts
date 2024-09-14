5f498600240c342134c01840f9058c6204c76ca3
0xWerz <mouomrane@gmail.com>
2024-09-14 01:00:01 +0100

    Fix Sentry environment detection

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,7 @@ class App {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
       integrations: [nodeProfilingIntegration()],
+      environment: process.env.NODE_ENV,
       // Tracing
       tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
