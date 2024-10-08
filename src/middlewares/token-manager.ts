b03d435395ceedaf312e53facaa13f7bd541ca40
0xWerz <mouomrane@gmail.com>
2024-10-08 16:03:44 +0100

    refactor: Update token expiration to 24 hours for better security

--- src/middlewares/token-manager.ts
+++ src/middlewares/token-manager.ts
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (user: any) => {
   return jwt.sign({ user }, process.env.JWT_SECRET_KEY as string, {
-    expiresIn: '3m',
+    expiresIn: '24h',
   });
 };
 
 export const generateRefreshToken = (user: any) => {
   return jwt.sign({ user }, process.env.JWT_REFRESH_SECRET_KEY as string, {
-    expiresIn: '24h',
+    expiresIn: '7d',
   });
 };
